a5ed1503ef535a9fd7048b48297222e57f6121cd
Jonathan King <jonpking@gmail.com>
2020-07-20 15:44:27 -0700

    added code to direct to public directory

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ mongoose.connect("mongodb://localhost:27017/yelp_camp", {
     .catch(err => console.log(error.message));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
 
 seedDB();
 
