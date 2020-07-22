2527d9a7eb0bfd08024cf1a4073ae5d37cdfd868
Jonathan King <jonpking@gmail.com>
2020-07-22 16:43:23 -0700

    added code for passport config as well as register get and post routes

--- app.js
+++ app.js
@@ -21,6 +21,18 @@ app.use(express.static(__dirname + "/public"));
 
 seedDB();
 
+// PASSPORT CONFIG
+app.use(require("express-session")({
+    secret: "Once again Rusty wins cutest dog!",
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 app.get("/", function (req, res) {
     res.render("landing");
 });
@@ -98,6 +110,30 @@ app.post("/campgrounds/:id/comments", function (req, res) {
             });
         }
     })
+});
+
+// =========================
+// AUTH ROUTES
+// =========================
+
+
+// show register form
+app.get("/register", function (req, res) {
+    res.render("register");
+});
+
+//handle sign up logic
+app.post("/register", function (req, res) {
+    const newUser = new User({ username: req.body.username });
+    User.register(newUser, req.body.password, function (err, user) {
+        if (err) {
+            console.log(err);
+            return res.render("register");
+        }
+        passport.authenticate("local")(req, res, function () {
+            res.redirect("/campgrounds");
+        });
+    });
 });
 
 app.listen(3000, function () {
