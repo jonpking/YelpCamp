6da09d9332257add69d3e619043969a07a0a32d7
Jonathan King <jonpking@gmail.com>
2020-07-22 16:53:41 -0700

    added routes for login page and login form submission

--- app.js
+++ app.js
@@ -136,6 +136,20 @@ app.post("/register", function (req, res) {
     });
 });
 
+// show login form
+app.get("/login", function (req, res) {
+    res.render("login");
+});
+
+// handling login logic
+app.post("/login", passport.authenticate("local",
+    {
+        successRedirect: "/campgrounds",
+        failureRedirect: "/login"
+    }), function (req, res) {
+
+    });
+
 app.listen(3000, function () {
     console.log("The YelpCamp server has started!");
 });
