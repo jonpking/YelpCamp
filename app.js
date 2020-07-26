f55c0f050afc36d83282d381611ce9ee78ec896c
Jonathan King <jonpking@gmail.com>
2020-07-26 12:41:52 -0700

    added method-override

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const seedDB = require("./seeds");
 const passport = require("passport");
 const LocalStrategy = require("passport-local");
 const User = require("./models/user");
+const methodOverride = require("method-override");
 
 // requiring routes
 const commentRoutes = require("./routes/comments");
@@ -16,13 +17,15 @@ const indexRoutes = require("./routes/index");
 
 mongoose.connect("mongodb://localhost:27017/yelp_camp", {
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
+    useFindAndModify: false
 })
     .then(() => console.log("Connected to DB!"))
     .catch(err => console.log(error.message));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
+app.use(methodOverride("_method"));
 
 // seed the database
 // seedDB();
