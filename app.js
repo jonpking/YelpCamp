9b807cd95ff8596e4415ef8ab7e29a8c0a041d26
Jonathan King <jonpking@gmail.com>
2020-07-26 12:00:18 -0700

    refactored code to automatically display username (no manual author field) when posting a comment

--- app.js
+++ app.js
@@ -24,7 +24,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 
-seedDB();
+// seed the database
+// seedDB();
 
 // PASSPORT CONFIG
 app.use(require("express-session")({
