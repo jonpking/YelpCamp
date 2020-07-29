a35ccf61c2d234b52bd764ec6f857b4a663ff4e3
Jonathan King <jonpking@gmail.com>
2020-07-29 14:32:52 -0700

    updated pricing on campgrounds to be provided by user on creation

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -17,13 +17,14 @@ router.get("/", function (req, res) {
 // CREATE route
 router.post("/", middleware.isLoggedIn, function (req, res) {
     const name = req.body.name;
+    const price = req.body.price;
     const image = req.body.image;
     const desc = req.body.description;
     const author = {
         id: req.user._id,
         username: req.user.username
     }
-    const newCampground = { name: name, image: image, description: desc, author: author };
+    const newCampground = { name: name, price: price, image: image, description: desc, author: author };
     Campground.create(newCampground, function (err, newlyCreated) {
         if (err) {
             console.log(err);
